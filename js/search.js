779ed879658b5f325f40f7482a767a48ce1c25de
dianguito <yaniqueorlando0@gmail.com>
2024-12-10 14:35:25 -0400

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,10 @@
+var buscador = $("#table").DataTable();
+
+$("#input-search").keyup(function(){
+    buscador.search($(this).val()).draw();
+    if ($("#input-search").val() == ""){
+        $(".content-search").fadeOut();
+    }else{
+        $(".content-search").fadeIn();
+    }
+})
